dfbe41670505bdca8ad69125a4adaa11a695e777
SaiiScrump <Saiileelavidee@gmail.com>
2019-10-01 22:02:04 +0700

    Finally done!!

---
+++ js/trial.js
@@ -0,0 +1,12 @@
+// ========================================================================= //
+//                                  Typed Js
+// ========================================================================= //
+var typed = $(".typed");
+
+    $(function () {
+        typed.typed({
+            strings: ["Leelavadee Chimchio.", " a Student.", "also a Traveler."],
+            typeSpeed: 50,
+            loop: true,
+        });
+    });
